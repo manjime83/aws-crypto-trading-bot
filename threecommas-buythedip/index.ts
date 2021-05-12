e75897c23711cc0d3ef051a2538ae543e84d3d0c
Manuel Jiménez <man.jime@gmail.com>
2021-05-12 17:22:29 -0500

    preparacion para el cambio de strategia

--- threecommas-buythedip/index.ts
+++ threecommas-buythedip/index.ts
@@ -2,64 +2,44 @@ import { Handler } from "aws-lambda";
 const threeCommasAPI = require("3commas-api-node");
 import Binance, { CandleChartInterval } from "binance-api-node";
 import { RSI, CrossUp } from "technicalindicators";
+// import Big from "big.js";
+
+import { DealType, OrderType, SafetyOrderType } from "./types";
 
 const { APIKEY, SECRET, RSI_PERIOD, RSI_OVERSOLD } = process.env;
 const api = new threeCommasAPI({ apiKey: APIKEY!, apiSecret: SECRET! });
 const binance = Binance();
 
-interface DealType {
-  id: number;
-  from_currency: string;
-  to_currency: string;
-  actual_profit_percentage: string;
-}
-
-interface SafetyOrderType {
-  order_id: string;
-  deal_order_type: string;
-  status_string: string;
-  updated_at: string;
-  quantity: string;
-  total: string;
-  average_price: string;
-}
-
-interface ResponseType extends SafetyOrderType {
-  deal_id: string;
-  from_currency: string;
-  to_currency: string;
-  error_description: string;
-  error: string;
-}
-
 const isOversold = async (symbol: string) => {
   const candles = await binance.candles({ symbol, interval: CandleChartInterval.FIVE_MINUTES });
   const rsi = RSI.calculate({ values: candles.map((candle) => +candle.close), period: +RSI_PERIOD! });
   const crossUp = CrossUp.calculate({ lineA: rsi, lineB: new Array(rsi.length).fill(+RSI_OVERSOLD!) });
-  console.debug(symbol, JSON.stringify(rsi.slice(-10)), JSON.stringify(crossUp.slice(-10)));
+  // console.debug(symbol, JSON.stringify(rsi.slice(-10)), JSON.stringify(crossUp.slice(-10)));
   return crossUp[crossUp.length - 1];
 };
 
 const TEN_MINUTES = 10 * 60 * 1000;
 
-const buyTheDip = async ({ id: deal_id, to_currency, from_currency }: DealType) => {
-  const safetyOrders: [SafetyOrderType] = await api.getDealSafetyOrders(deal_id);
+const buyTheDip = async (deal: DealType) => {
+  const dealSafetyOrders: [OrderType] = await api.getDealSafetyOrders(deal.id);
 
-  if (Array.isArray(safetyOrders)) {
-    const lastFilledSafetyOrder = safetyOrders
-      .filter((so) => ["Base", "Safety", "Manual Safety"].includes(so.deal_order_type) && so.status_string === "Filled")
+  if (dealSafetyOrders) {
+    const lastFilledSafetyOrder = dealSafetyOrders
+      .filter((so) => ["Base", "Manual Safety"].includes(so.deal_order_type) && so.status_string === "Filled")
       .reduce((prev, curr) => (Date.parse(prev.updated_at) > Date.parse(curr.updated_at) ? prev : curr));
 
+    console.info(deal, lastFilledSafetyOrder);
+
     if (Date.now() - Date.parse(lastFilledSafetyOrder.updated_at) > TEN_MINUTES) {
-      const symbol = to_currency.concat(from_currency);
+      const symbol = deal.to_currency.concat(deal.from_currency);
       if (await isOversold(symbol)) {
         const order = await api.dealAddFunds({
           quantity: +lastFilledSafetyOrder.quantity,
           is_market: true,
           response_type: "market_order",
-          deal_id,
+          deal_id: deal.id,
         });
-        return { ...order, deal_id, to_currency, from_currency } as ResponseType;
+        return { deal, order } as SafetyOrderType;
       }
     }
   }
@@ -70,33 +50,29 @@ const buyTheDip = async ({ id: deal_id, to_currency, from_currency }: DealType) 
 export const handler: Handler<{}> = async () => {
   const deals: [DealType] = await api.getDeals({ scope: "active" });
 
-  if (Array.isArray(deals)) {
+  if (deals) {
     const orders = await Promise.all(
       deals.filter((deal) => +deal.actual_profit_percentage < 0).map((deal) => buyTheDip(deal))
     );
 
-    orders
-      .filter((order) => order !== undefined)
-      .forEach((order) => {
-        if (order!.order_id) {
-          console.info(
-            `Manual safety order placed (${order!.order_id}). Rate: ${order!.average_price} ${order!.to_currency.concat(
-              order!.from_currency
-            )}, Amount: ${order!.quantity} ${order!.to_currency}, Volume: ${order!.total} ${
-              order!.from_currency
-            }. https://3commas.io/deals/${order!.deal_id}`
-          );
-        } else {
-          console.error(
-            `Error placing safety order (${order!.to_currency.concat(order!.from_currency)}). ${
-              order!.error_description
-            }. https://3commas.io/deals/${order!.deal_id}`
-          );
-        }
-      });
+    (orders.filter((order) => order !== undefined) as [SafetyOrderType]).forEach(({ deal, order }) => {
+      if (order.order_id) {
+        console.info(
+          `Manual safety order placed (${order.order_id}). Rate: ${order.average_price} ${deal.to_currency.concat(
+            deal.from_currency
+          )}, Amount: ${order.quantity} ${deal.to_currency}, Volume: ${order.total} ${
+            deal.from_currency
+          }. https://3commas.io/deals/${deal.id}`
+        );
+      } else {
+        console.error(
+          `Error placing safety order (${deal.to_currency.concat(deal.from_currency)}). ${
+            order.error_description
+          }. https://3commas.io/deals/${deal.id}`
+        );
+      }
+    });
   }
 };
 
-/* CloudWatch Logs Insights query
-filter @message like /3commas.io/ | fields @timestamp, @message | sort @timestamp desc | limit 100
-*/
+// CloudWatch Logs Insights Query: filter @message like /3commas.io/ | fields @message | sort @timestamp desc | limit 100
---
+++ threecommas-buythedip/types.ts
@@ -0,0 +1,94 @@
+export interface DealType {
+  id: number;
+  type: string;
+  bot_id: number;
+  max_safety_orders: number;
+  deal_has_error: boolean;
+  from_currency_id: number;
+  to_currency_id: number;
+  account_id: number;
+  active_safety_orders_count: number;
+  created_at: string;
+  updated_at: string;
+  closed_at: null;
+  "finished?": false;
+  current_active_safety_orders_count: number;
+  current_active_safety_orders: number;
+  completed_safety_orders_count: number;
+  completed_manual_safety_orders_count: number;
+  "cancellable?": boolean;
+  "panic_sellable?": boolean;
+  trailing_enabled: boolean;
+  tsl_enabled: boolean;
+  stop_loss_timeout_enabled: boolean;
+  stop_loss_timeout_in_seconds: number;
+  active_manual_safety_orders: number;
+  pair: string;
+  status: string;
+  localized_status: string;
+  take_profit: string;
+  base_order_volume: string;
+  safety_order_volume: string;
+  safety_order_step_percentage: string;
+  leverage_type: string;
+  leverage_custom_value: string;
+  bought_amount: string;
+  bought_volume: string;
+  bought_average_price: string;
+  base_order_average_price: string;
+  sold_amount: string;
+  sold_volume: string;
+  sold_average_price: string;
+  take_profit_type: string;
+  final_profit: string;
+  martingale_coefficient: string;
+  martingale_volume_coefficient: string;
+  martingale_step_coefficient: string;
+  stop_loss_percentage: string;
+  error_message: string;
+  profit_currency: string;
+  stop_loss_type: string;
+  safety_order_volume_type: string;
+  base_order_volume_type: string;
+  from_currency: string;
+  to_currency: string;
+  current_price: string;
+  take_profit_price: string;
+  stop_loss_price: string;
+  final_profit_percentage: string;
+  actual_profit_percentage: string;
+  bot_name: string;
+  account_name: string;
+  usd_final_profit: string;
+  actual_profit: string;
+  actual_usd_profit: string;
+  failed_message: string;
+  reserved_base_coin: string;
+  reserved_second_coin: string;
+  trailing_deviation: string;
+  trailing_max_price: string;
+  tsl_max_price: string;
+  strategy: string;
+}
+
+export interface OrderType {
+  order_id: string;
+  order_type: string;
+  deal_order_type: string;
+  cancellable: boolean;
+  status_string: string;
+  created_at: string;
+  updated_at: string;
+  quantity: string;
+  quantity_remaining: string;
+  total: string;
+  rate: string;
+  average_price: string;
+  error_description?: string;
+  error?: string;
+}
+
+export interface SafetyOrderType {
+  deal: DealType;
+  order: OrderType;
+}
