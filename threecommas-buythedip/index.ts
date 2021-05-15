fe655d723f70a9921ca3f55e387af4fd2309459a
Manuel Jiménez <man.jime@gmail.com>
2021-05-15 10:42:54 -0500

    cambios

--- threecommas-buythedip/index.ts
+++ threecommas-buythedip/index.ts
@@ -2,9 +2,9 @@ import { Handler } from "aws-lambda";
 const threecommas_api_node_1 = require("3commas-api-node");
 import Binance, { CandleChartInterval } from "binance-api-node";
 import { RSI, CrossUp } from "technicalindicators";
-import Big from "big.js";
+import Decimal from "decimal.js";
 
-import { DealType, SafetyOrderType } from "./types";
+import { DealType, OrderType, SafetyOrderType } from "./types";
 
 const { APIKEY, SECRET, RSI_PERIOD, RSI_OVERSOLD } = process.env;
 const api = new threecommas_api_node_1({ apiKey: APIKEY!, apiSecret: SECRET! });
@@ -22,17 +22,54 @@ const oversold = async (symbol: string) => {
   }
 };
 
-const buyTheDip = async (deal: DealType) => {
-  const deviation = new Big(deal.martingale_step_coefficient).eq(1)
-    ? new Big(deal.safety_order_step_percentage).times(deal.completed_manual_safety_orders_count + 1)
-    : new Big(deal.safety_order_step_percentage)
-        .times(new Big(deal.martingale_step_coefficient).pow(deal.completed_manual_safety_orders_count + 1).minus(1))
-        .div(new Big(deal.martingale_step_coefficient).minus(1));
-  const maxBuyPrice = new Big(deal.base_order_average_price).times(new Big(100).minus(deviation).div(100));
+const TEN_MINUTES = 10 * 60 * 1000;
 
-  // const totalQuantity = new Big(2)
-  //   .pow(Math.min(deal.completed_manual_safety_orders_count, deal.max_safety_orders))
-  //   .times(deal.safety_order_volume);
+const buyTheDip = async (deal: DealType) => {
+  const orders = (await api.getDealSafetyOrders(deal.id)) as [OrderType];
+  if (!orders) return undefined;
+
+  const manualOrders = orders.filter(
+    ({ deal_order_type, status_string }) =>
+      ["Base", "Manual Safety"].includes(deal_order_type) && status_string === "Filled"
+  );
+
+  // const baseOrder = manualOrders[manualOrders.length - 1];
+  const lastOrder = manualOrders[0];
+  const totalQuantity = manualOrders.reduce((prev, curr) => prev.plus(curr.quantity), new Decimal(0)).toNumber();
+
+  console.debug(
+    deal.to_currency.concat(deal.from_currency),
+    +deal.current_price > +lastOrder.average_price,
+    Date.now() - Date.parse(lastOrder.updated_at) < TEN_MINUTES,
+    totalQuantity
+  );
+
+  if (
+    +deal.current_price > +lastOrder.average_price ||
+    Date.now() - Date.parse(lastOrder.updated_at) < TEN_MINUTES ||
+    !(await oversold(deal.to_currency.concat(deal.from_currency)))
+  )
+    return undefined;
+
+  const order = await api.dealAddFunds({
+    quantity: totalQuantity,
+    is_market: true,
+    response_type: "market_order",
+    deal_id: deal.id,
+  });
+
+  return { deal, order } as SafetyOrderType;
+};
+
+const buyTheDipAngel = async (deal: DealType) => {
+  const deviation = new Decimal(deal.martingale_step_coefficient).eq(1)
+    ? new Decimal(deal.safety_order_step_percentage).times(deal.completed_manual_safety_orders_count + 1)
+    : new Decimal(deal.safety_order_step_percentage)
+        .times(
+          new Decimal(deal.martingale_step_coefficient).pow(deal.completed_manual_safety_orders_count + 1).minus(1)
+        )
+        .div(new Decimal(deal.martingale_step_coefficient).minus(1));
+  const maxBuyPrice = new Decimal(deal.base_order_average_price).times(new Decimal(100).minus(deviation).div(100));
 
   console.debug(
     deal.to_currency.concat(deal.from_currency),
@@ -56,31 +93,33 @@ const buyTheDip = async (deal: DealType) => {
 };
 
 export const handler: Handler<{}> = async () => {
-  const deals: [DealType] = await api.getDeals({ scope: "active" });
+  const deals = (await api.getDeals({ scope: "active" })) as [DealType];
+  if (!deals) return;
 
-  if (deals) {
-    const orders = await Promise.all(
-      deals.filter((deal) => +deal.actual_profit_percentage < 0).map((deal) => buyTheDip(deal))
-    );
+  const orders = await Promise.all(
+    deals
+      .filter((deal) => +deal.actual_profit_percentage < 0)
+      // .filter((deal) => deal.to_currency.concat(deal.from_currency) === "YFIUSDT")
+      .map((deal) => buyTheDip(deal))
+  );
 
-    (orders.filter((order) => order !== undefined) as [SafetyOrderType]).forEach(({ deal, order }) => {
-      if (order.order_id) {
-        console.info(
-          `Manual safety order placed (${order.order_id}). Rate: ${order.average_price} ${deal.to_currency.concat(
-            deal.from_currency
-          )}, Amount: ${order.quantity} ${deal.to_currency}, Volume: ${order.total} ${
-            deal.from_currency
-          }. https://3commas.io/deals/${deal.id}`
-        );
-      } else {
-        console.error(
-          `Error placing safety order (${deal.to_currency.concat(deal.from_currency)}). ${
-            order.error_description
-          }. https://3commas.io/deals/${deal.id}`
-        );
-      }
-    });
-  }
+  (orders.filter((order) => order !== undefined) as [SafetyOrderType]).forEach(({ deal, order }) => {
+    if (order.order_id) {
+      console.info(
+        `Manual safety order placed (${order.order_id}). Rate: ${order.average_price} ${deal.to_currency.concat(
+          deal.from_currency
+        )}, Amount: ${order.quantity} ${deal.to_currency}, Volume: ${order.total} ${
+          deal.from_currency
+        }. https://3commas.io/deals/${deal.id}`
+      );
+    } else {
+      console.error(
+        `Error placing safety order (${deal.to_currency.concat(deal.from_currency)}). ${
+          order.error_description
+        }. https://3commas.io/deals/${deal.id}`
+      );
+    }
+  });
 };
 
 // CloudWatch Logs Insights Query: filter @message like /3commas.io/ | fields @message | sort @timestamp desc | limit 100
